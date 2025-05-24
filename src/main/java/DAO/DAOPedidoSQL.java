c1781269e15e248ba0ef9f0dd9f9450681a7d499
Ahmed <ahmedlb26205@gmail.com>
2025-05-24 22:26:25 +0200

    Creando clases DAO

---
+++ src/main/java/DAO/DAOPedidoSQL.java
@@ -0,0 +1,171 @@
+package DAO;
+
+import models.Cliente;
+import models.Pedido;
+import models.Trabajador;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class DAOPedidoSQL implements DAOPedido {
+
+    @Override
+    public ArrayList<Pedido> readAll(DAOManager dao) {
+        ArrayList<Pedido> pedidos = new ArrayList<>();
+        String sentencia = "SELECT * FROM Pedido";
+        try {
+            dao.open();
+            PreparedStatement ps = dao.getConn().prepareStatement(sentencia);
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    pedidos.add(new Pedido(
+                            rs.getInt("id"),
+                            rs.getDate("fechaPedido").toLocalDate(),
+                            rs.getDate("fechaEntregaEstimada").toLocalDate(),
+                            rs.getInt("estado"),
+                            rs.getString("comentario"),
+                            daoPedidoProductos.readAll(dao, rs.getInt("id"))
+                    ));
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                dao.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return pedidos;
+    }
+
+    @Override
+    public ArrayList<Pedido> readPedidosByIdTrabajador(DAOManager dao, Trabajador trabajador) {
+        ArrayList<Pedido> pedidos = new ArrayList<>();
+        String sentencia = "SELECT * FROM Pedido WHERE `idTrabajador` = '" + trabajador.getId() + "'";
+        try {
+            dao.open();
+            PreparedStatement ps = dao.getConn().prepareStatement(sentencia);
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    pedidos.add(new Pedido(
+                            rs.getInt("id"),
+                            rs.getDate("fechaPedido").toLocalDate(),
+                            rs.getDate("fechaEntregaEstimada").toLocalDate(),
+                            rs.getInt("estado"),
+                            rs.getString("comentario"),
+                            daoPedidoProductos.readAll(dao, rs.getInt("id"))
+                    ));
+                }
+            }
+        } catch (Exception e) {
+            return pedidos;
+        } finally {
+            try {
+                dao.close();
+            } catch (SQLException e) {
+                return pedidos;
+            }
+        }
+        return pedidos;
+    }
+
+    @Override
+    public boolean insert(DAOManager dao, Pedido pedido, Cliente cliente) {
+        try {
+            dao.open();
+            String sentencia = "INSERT INTO `Pedido` (`id`, `fechaPedido`, `fechaEntregaEstimada`, `estado`, `comentario`, " +
+                    "`idCliente`) VALUES ('" + pedido.getId() + "', '" + pedido.getFechaPedido() + "', '" +
+                    pedido.getFechaEntregaEstimada() + "', '" + pedido.getEstado() + "', '" + pedido.getComentario() +
+                    "', '" + cliente.getId() + "')";
+            Statement stmt = dao.getConn().createStatement();
+            stmt.executeUpdate(sentencia);
+            return true;
+        } catch (Exception e) {
+            return false;
+        } finally {
+            try {
+                dao.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public boolean update(DAOManager dao, Pedido pedido) {
+        try {
+            dao.open();
+            String sentencia = "UPDATE `Pedido` SET `fechaPedido` = '" + pedido.getFechaPedido() + "', `fechaEntregaEstimada` = '"
+                    + pedido.getFechaEntregaEstimada() + "', `estado` = '" + pedido.getEstado() + "', `comentario` = '" +
+                    pedido.getComentario() + "' WHERE `Pedido`.`id` = " + pedido.getId();
+            Statement stmt = dao.getConn().createStatement();
+            stmt.executeUpdate(sentencia);
+            return true;
+        } catch (Exception e) {
+            return false;
+        } finally {
+            try {
+                dao.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public boolean updateTrabajador(DAOManager dao, Pedido pedido, Trabajador trabajador) {
+        try {
+            dao.open();
+            String sentencia = "UPDATE `Pedido` SET `idTrabajador` = '" + trabajador.getId() +
+                    "' WHERE `Pedido`.`id` = " + pedido.getId();
+            Statement stmt = dao.getConn().createStatement();
+            stmt.executeUpdate(sentencia);
+            return true;
+        } catch (Exception e) {
+            return false;
+        } finally {
+            try {
+                dao.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public ArrayList<Pedido> readPedidosByIdCliente(DAOManager dao, Cliente c) {
+        ArrayList<Pedido> pedidos = new ArrayList<>();
+        String sentencia = "SELECT * FROM Pedido WHERE `idCliente` = '" + cliente.getId() + "'";
+
+        try {
+            dao.open();
+            PreparedStatement ps = dao.getConn().prepareStatement(sentencia);
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    pedidos.add(new Pedido(
+                            rs.getInt("id"),
+                            rs.getDate("fechaPedido").toLocalDate(),
+                            rs.getDate("fechaEntregaEstimada").toLocalDate(),
+                            rs.getInt("estado"),
+                            rs.getString("comentario"),
+                            daoPedidoProductos.readAll(dao, rs.getInt("id"))
+                    ));
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                dao.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return pedidos;
+    }
+}
---
+++ src/main/java/DAO/DAOTrabajadorSQL.java
@@ -0,0 +1,98 @@
+package DAO;
+
+import models.Trabajador;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class DAOTrabajadorSQL implements DAOTrabajador {
+
+    @Override
+    public ArrayList<Trabajador> readAll(DAOManager dao) {
+        ArrayList<Trabajador> lista = new ArrayList<>();
+        String sentencia = "SELECT * FROM Trabajador";
+
+        try {
+            dao.open();
+            PreparedStatement ps = dao.getConn().prepareStatement(sentencia);
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    lista.add(new Trabajador(rs.getInt("id"), rs.getString("nombre"), rs.getString("pass"), rs.getString("email"), rs.getInt("movil")));
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                dao.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        for (Trabajador t : lista) {
+            t.setPedidosAsignados(daoPedidoSQL.readPedidosByIdTrabajador(dao, t));
+            //t.getPedidosAsignados().addAll(daoPedidoSQL.readPedidosByIdTrabajador(dao, t));
+        }
+        return lista;
+    }
+
+    @Override
+    public boolean insert(DAOManager dao, Trabajador trabajador) {
+        try {
+            dao.open();
+            String sentencia = "INSERT INTO `Trabajador` (`id`, `nombre`, `pass`, `email`, `movil`) VALUES ('" + trabajador.getId() + "', '" + trabajador.getNombre() + "', '" + trabajador.getPass() + "', '" + trabajador.getEmail() + "', '" + trabajador.getMovil() + "')";
+            Statement stmt = dao.getConn().createStatement();
+            stmt.executeUpdate(sentencia);
+            return true;
+        } catch (Exception e) {
+            return false;
+        } finally {
+            try {
+                dao.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public boolean update(DAOManager dao, Trabajador trabajador) {
+        try {
+            dao.open();
+            String sentencia = "UPDATE Trabajador SET `nombre` = '" + trabajador.getNombre() + "', `pass` = '" + trabajador.getPass() + "', `email` = '" + trabajador.getEmail() + "', `movil` = '" + trabajador.getMovil() + "' WHERE `Trabajador`.`id` = " + trabajador.getId();
+            Statement stmt = dao.getConn().createStatement();
+            stmt.executeUpdate(sentencia);
+            return true;
+        } catch (Exception e) {
+            return false;
+        } finally {
+            try {
+                dao.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public boolean delete(DAOManager dao, Trabajador trabajador) {
+        try {
+            dao.open();
+            String sentencia = "DELETE FROM Trabajador WHERE `Trabajador`.`id` = '" + trabajador.getId() + "'";
+            Statement stmt = dao.getConn().createStatement();
+            stmt.executeUpdate(sentencia);
+            return true;
+        } catch (Exception e) {
+            return false;
+        } finally {
+            try {
+                dao.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
