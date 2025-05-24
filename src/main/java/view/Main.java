c133508b530a95f4e5a99b5fb15ace913c0260f1
Ahmed <ahmedlb26205@gmail.com>
2025-05-25 00:11:39 +0200

    Arreglando metodos Controlador

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -61,8 +61,6 @@ public class Main {
             System.out.println("____________________________________________________________________________________________________");
             Utils.pulsaParaContinuar();
         }
-
-
         if (clientesVacios) {
             String respuesta;
             do {
